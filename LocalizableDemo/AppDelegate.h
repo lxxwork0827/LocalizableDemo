5200b0f250d472d278a3710fa8f8680aa747ac62
刘星星 <lxxwork0827@163.com>
2016-06-24 18:50:00 +0800

    关于XCode7 应用本地化的Demo
    尚未完善

--- LocalizableDemo/AppDelegate.h
+++ LocalizableDemo/AppDelegate.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
+ #define Localized(key)  [[NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"%@",[[NSUserDefaults standardUserDefaults] objectForKey:@"appLanguage"]] ofType:@"lproj"]] localizedStringForKey:(key) value:nil table:nil]
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
--- LocalizableDemo/ViewController.m
+++ LocalizableDemo/ViewController.m
@@ -7,21 +7,23 @@
 //
 
 #import "ViewController.h"
-
+#import "AppDelegate.h"
 @interface ViewController ()
-
+//http://blog.csdn.net/lmyuanhang/article/details/50419971
 @end
 
 @implementation ViewController
 
+/**
+ 使用步骤：
+ 首先先配置语言环境，然后创建相对应的strings
+ */
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    UILabel *label1 = [[UILabel alloc]initWithFrame:CGRectMake(100, 300, 200, 100)];
+    label1.backgroundColor = [UIColor redColor];
+    label1.text = Localized(@"这是中文环境哦");
+    [self.view addSubview:label1];
 }
 
 @end
